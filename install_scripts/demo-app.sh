1f982b3979c5112cc8b83e3acf57066b379abbc9
Esmé Cowles <escowles@ticklefish.org>
2016-09-23 07:15:37 -0400

    Sufia 7.x master

--- install_scripts/demo-app.sh
+++ install_scripts/demo-app.sh
@@ -30,10 +30,11 @@ echo "Creating Sufia demo in /home/vagrant/sufia-demo"
 cd
 rails new sufia-demo --skip-spring
 cd sufia-demo
-echo "gem 'sufia', '7.0.0.beta4'" >> Gemfile
+echo "gem 'sufia', github: 'projecthydra/sufia', branch: :master" >> Gemfile
+echo "gem 'flipflop', github: 'jcoyne/flipflop', branch: :hydra" >> Gemfile
 echo "gem 'unicorn-rails'" >> Gemfile
 bundle install --quiet
 rails generate sufia:install -f -q
-#rails generate sufia:work Work -q # TODO: uncomment this when next release > beta4 is out
+rails generate sufia:work Work -q
 rake db:migrate
 echo "$DEMO_TASK" >> Rakefile
